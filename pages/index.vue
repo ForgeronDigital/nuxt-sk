f213b74466e75fb66ca314f4352c91dafc487c4e
Fernandez <esteban.f1994@gmail.com>
2020-12-21 09:42:27 +0100

    init

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">fondasol</h1>
+      <h1 class="title">Nuxt Starter Kit(kat)</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
